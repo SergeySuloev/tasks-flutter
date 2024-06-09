37061f51473456ba171a252877ba580cdc089c66
Sergey <super.suloev@gmail.com>
2024-06-09 21:12:34 +0300

    Added Hive DB to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'tasklib.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initHive();
   runApp(const TaskAppHomescreen());
 }
 
@@ -16,31 +19,43 @@ class TaskAppHomescreen extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         useMaterial3: true,
       ),
-      home: TasksList(title: 'Tasks'),
+      home: const TasksList(title: 'Tasks'),
     );
   }
 }
 
-// ignore: must_be_immutable
 class TasksList extends StatefulWidget {
   final String title;
-  List<Task> tasks = <Task>[];
-  TasksList({super.key, required this.title});
+  const TasksList({super.key, required this.title});
 
   @override
   State<TasksList> createState() => _TasksListState();
 }
 
 class _TasksListState extends State<TasksList> {
+  late Completer<List<int>> taskKeysCompleter;
+
+  void completeKeysCompleter() {
+    taskKeysCompleter = Completer<List<int>>();
+    taskKeysCompleter.complete(getAllTaskKeys());
+  }
+
   @override
   void initState() {
     super.initState();
+    completeKeysCompleter();
   }
 
-  void removeTask(int index) {
-    setState(() {
-      widget.tasks.removeAt(index);
-    });
+  Future<void> removeTask(int taskKey) async {
+    await removeTaskFromDB(taskKey);
+    completeKeysCompleter();
+    setState(() {});
+  }
+
+  Future<void> toggleTask(int taskKey) async {
+    await toggleTaskInHive(taskKey);
+    completeKeysCompleter();
+    setState(() {});
   }
 
   @override
@@ -50,13 +65,43 @@ class _TasksListState extends State<TasksList> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: ListView.builder(
-        itemCount: widget.tasks.length,
-        itemBuilder: (BuildContext context, int index) {
-          var task = widget.tasks[index];
-          return TaskCard(removeTask: () => removeTask(index), taskVar: task);
-        },
-      ),
+      body: FutureBuilder<List<int>>(
+          future: taskKeysCompleter.future,
+          builder: (BuildContext context, AsyncSnapshot<List<int>> snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final taskIndex = snapshot.data![index];
+                  return FutureBuilder<Task?>(
+                    future: getTask(taskIndex),
+                    builder:
+                        (BuildContext context, AsyncSnapshot<Task?> snapshot) {
+                      if (snapshot.hasData) {
+                        return TaskCard(
+                          removeTask: () async {
+                            await removeTask(taskIndex);
+                          },
+                          toggleTask: () async {
+                            await toggleTask(taskIndex);
+                          },
+                          taskKey: taskIndex,
+                        );
+                      } else if (snapshot.hasError) {
+                        return Text('Error: ${snapshot.error}');
+                      } else {
+                        return const CircularProgressIndicator();
+                      }
+                    },
+                  );
+                },
+              );
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else {
+              return const CircularProgressIndicator();
+            }
+          }),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           final result = await Navigator.push<String>(
@@ -65,7 +110,9 @@ class _TasksListState extends State<TasksList> {
           );
           if (result != null) {
             setState(() {
-              widget.tasks.add(Task(taskString: result));
+              final newTask = Task(taskString: result);
+              addTaskToDB(newTask);
+              completeKeysCompleter();
             });
           }
         },
@@ -78,8 +125,13 @@ class _TasksListState extends State<TasksList> {
 
 class TaskCard extends StatefulWidget {
   final VoidCallback removeTask;
-  final Task taskVar;
-  const TaskCard({super.key, required this.removeTask, required this.taskVar});
+  final VoidCallback toggleTask;
+  final int taskKey;
+  const TaskCard(
+      {super.key,
+      required this.removeTask,
+      required this.taskKey,
+      required this.toggleTask});
 
   @override
   State<TaskCard> createState() => _TaskCardState();
@@ -89,18 +141,28 @@ class _TaskCardState extends State<TaskCard> {
   // ignore: prefer_const_constructors
   TextDecoration? strikeThrough;
   bool? isDoneVariable;
+  late Future<Task?> task;
+  late Task taskVar;
 
   @override
   void initState() {
     super.initState();
-    isDoneVariable = widget.taskVar.isDone;
-    strikeThrough = isDoneVariable! ? TextDecoration.lineThrough : null;
+    taskCreator();
+  }
+
+  Future<void> taskCreator() async {
+    task = getTask(widget.taskKey);
+    taskVar = (await task)!;
+    setState(() {
+      isDoneVariable = taskVar.isDone;
+      strikeThrough = isDoneVariable! ? TextDecoration.lineThrough : null;
+    });
   }
 
   void _toggleTask() {
     setState(() {
-      widget.taskVar.toggleTask();
-      isDoneVariable = widget.taskVar.isDone;
+      widget.toggleTask();
+      isDoneVariable = taskVar.isDone;
       strikeThrough = isDoneVariable! ? TextDecoration.lineThrough : null;
     });
   }
@@ -115,44 +177,58 @@ class _TaskCardState extends State<TaskCard> {
         textStyle.copyWith(fontSize: MediaQuery.of(context).size.width * 0.05);
     textStyle = textStyle.copyWith(decoration: strikeThrough);
 
-    return Card(
-      color: theme.colorScheme.primaryContainer,
-      child: InkWell(
-        onTap: () async {
-          final result = await Navigator.push<String>(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => TaskFullView(taskVar: widget.taskVar)));
-          if (result != null) {
-            setState(() => widget.taskVar.editTask(result));
-          }
-        },
-        child: Padding(
-          padding: const EdgeInsets.all(8),
-          child: Row(
-            children: [
-              Checkbox(
-                checkColor: theme.colorScheme.onPrimaryContainer,
-                value: isDoneVariable,
-                onChanged: (bool? value) {
-                  _toggleTask();
+    return FutureBuilder<Task?>(
+        future: task,
+        builder: (BuildContext context, AsyncSnapshot<Task?> snapshot) {
+          if (snapshot.hasData) {
+            return Card(
+              color: theme.colorScheme.primaryContainer,
+              child: InkWell(
+                onTap: () async {
+                  final result = await Navigator.push<String>(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              TaskFullView(taskKey: widget.taskKey)));
+                  if (result != null) {
+                    await editTaskInHive(widget.taskKey, result);
+                    setState(() {});
+                  }
                 },
-              ),
-              Expanded(
-                child: Text(
-                  widget.taskVar.taskString,
-                  style: textStyle,
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: MediaQuery.of(context).size.height ~/ 100,
+                child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Row(
+                    children: [
+                      Checkbox(
+                        tristate: true,
+                        checkColor: theme.colorScheme.onPrimaryContainer,
+                        value: isDoneVariable,
+                        onChanged: (bool? value) {
+                          _toggleTask();
+                        },
+                      ),
+                      Expanded(
+                        child: Text(
+                          taskVar.taskString,
+                          style: textStyle,
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: MediaQuery.of(context).size.height ~/ 100,
+                        ),
+                      ),
+                      ElevatedButton(
+                          onPressed: widget.removeTask,
+                          child: const Icon(Icons.remove))
+                    ],
+                  ),
                 ),
               ),
-              ElevatedButton(
-                  onPressed: widget.removeTask, child: const Icon(Icons.remove))
-            ],
-          ),
-        ),
-      ),
-    );
+            );
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else {
+            return const CircularProgressIndicator();
+          }
+        });
   }
 }
 
@@ -218,8 +294,8 @@ class _AddNewTaskState extends State<AddNewTask> {
 }
 
 class TaskFullView extends StatefulWidget {
-  final Task taskVar;
-  const TaskFullView({super.key, required this.taskVar});
+  final int taskKey;
+  const TaskFullView({super.key, required this.taskKey});
 
   @override
   State<TaskFullView> createState() => _TaskFullViewState();
@@ -227,52 +303,73 @@ class TaskFullView extends StatefulWidget {
 
 class _TaskFullViewState extends State<TaskFullView> {
   final _formKey = GlobalKey<FormState>();
-  var _controller = TextEditingController();
   final _focusNode = FocusNode();
+  var _controller = TextEditingController();
+  late Future<Task?> task;
 
   @override
   void initState() {
     super.initState();
-    _controller = TextEditingController(text: widget.taskVar.taskString);
+    initTask();
     _focusNode.requestFocus();
+  }
+
+  Future<void> initTask() async {
+    task = getTask(widget.taskKey);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: const Text('Edit task')),
-        body: Center(
-          child: SizedBox(
-            width: MediaQuery.sizeOf(context).width * 0.8,
-            height: MediaQuery.sizeOf(context).height * 0.3,
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  TextFormField(
-                    focusNode: _focusNode,
-                    controller: _controller,
-                    decoration: const InputDecoration(labelText: 'Task'),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter a task';
-                      }
-                      return null;
-                    },
+    return FutureBuilder<Task?>(
+        future: task,
+        builder: (BuildContext context, AsyncSnapshot<Task?> snapshot) {
+          if (snapshot.hasData) {
+            if (snapshot.data != null) {
+              _controller.text = snapshot.data!.taskString;
+            } else {
+              _controller = TextEditingController();
+            }
+            return Scaffold(
+                appBar: AppBar(title: const Text('Edit task')),
+                body: Center(
+                  child: SizedBox(
+                    width: MediaQuery.sizeOf(context).width * 0.8,
+                    height: MediaQuery.sizeOf(context).height * 0.3,
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            focusNode: _focusNode,
+                            controller: _controller,
+                            decoration:
+                                const InputDecoration(labelText: 'Task'),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter a task';
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(height: 16),
+                          ElevatedButton(
+                              onPressed: () {
+                                if (_formKey.currentState!.validate()) {
+                                  _focusNode.unfocus();
+                                  Navigator.pop(context, _controller.text);
+                                }
+                              },
+                              child: const Text('Edit task'))
+                        ],
+                      ),
+                    ),
                   ),
-                  const SizedBox(height: 16),
-                  ElevatedButton(
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          _focusNode.unfocus();
-                          Navigator.pop(context, _controller.text);
-                        }
-                      },
-                      child: const Text('Edit task'))
-                ],
-              ),
-            ),
-          ),
-        ));
+                ));
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else {
+            return const CircularProgressIndicator();
+          }
+        });
   }
 }
--- lib/tasklib.dart
+++ lib/tasklib.dart
@@ -1,5 +1,14 @@
+import 'package:flutter/foundation.dart';
+import 'package:hive_flutter/adapters.dart';
+
+part 'tasklib.g.dart';
+
+@HiveType(typeId: 0)
 class Task {
+  @HiveField(0)
   bool isDone;
+
+  @HiveField(1)
   String taskString;
 
   Task({this.isDone = false, required this.taskString});
@@ -15,4 +24,63 @@ class Task {
   void editTask(newTaskString) {
     taskString = newTaskString;
   }
+}
+
+late Box<Task> tasksBox;
+
+Future<void> openHiveBox() async {
+  tasksBox = await Hive.openBox('tasks');
+}
+
+Future<void> initHive() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(TaskAdapter());
+  await openHiveBox();
+}
+
+Future<int> addTaskToDB(Task task) async {
+  final taskKey = tasksBox.add(task);
+  return taskKey;
+}
+
+Future<List<Task>> readAllTasks() async {
+  final List<Task> tasks = tasksBox.values.cast<Task>().toList();
+  return tasks;
+}
+
+Future<void> removeTaskFromDB(int taskKey) async {
+  try {
+    await tasksBox.delete(taskKey);
+  } catch (e) {
+    if (kDebugMode) {
+      print('Error: $e');
+    }
+  }
+}
+
+Future<Task?> getTask(int taskKey) async {
+  final task = tasksBox.get(taskKey);
+  return task;
+}
+
+Future<List<int>> getAllTaskKeys() async {
+  final List<int> keys = tasksBox.keys.cast<int>().toList();
+  return keys;
+}
+
+Future<void> toggleTaskInHive(int taskKey) async {
+  final task = tasksBox.get(taskKey);
+  task!.toggleTask();
+  await tasksBox.put(taskKey, task);
+}
+
+Future<void> editTaskInHive(int taskKey, String newTask) async {
+  final task = tasksBox.get(taskKey);
+  task!.editTask(newTask);
+  await tasksBox.put(taskKey, task);
+}
+
+Future<void> closeHive() async {
+  await tasksBox.close();
+  await Hive.close();
 }
